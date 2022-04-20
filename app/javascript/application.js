6533d947e29bbcdc2827a2bd4ec478d10a0dc3cd
Darren Boyd <darren.boyd@gmail.com>
2022-04-20 11:48:57 -0700

    Fix stimulus controller register

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,10 +11,26 @@ import LocalTime from "local-time"
 import Farming from "./js/farming"
 
 import { Application } from "@hotwired/stimulus"
-
 const application = Application.start()
-const context = require.context("controllers", true, /.js$/)
-application.load(definitionsFromContext(context))
+
+import ItemFormController from "./controllers/item_form_controller"
+import LunarRiftController from "./controllers/lunar_rift_controller"
+import MoreLinkController from "./controllers/more-link-controller"
+import RecipeController from "./controllers/recipe-controller"
+import SelectNavController from "./controllers/select-nav-controller"
+import SkillsBasicsController from "./controllers/skills_basics_controller"
+import SkillsController from "./controllers/skills-controller"
+import SkillsRollupController from "./controllers/skills-rollup-controller"
+
+application.register("item-form", ItemFormController)
+application.register("lunar-rift", LunarRiftController)
+application.register("more-link", MoreLinkController)
+application.register("recipe", RecipeController)
+application.register("select-nav", SelectNavController)
+application.register("skills-basics", SkillsBasicsController)
+application.register("skills", SkillsController)
+application.register("skills-rollup", SkillsRollupController)
+
 application.register('autocomplete', Autocomplete)
 
 LocalTime.start()
