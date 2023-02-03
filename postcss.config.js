bdb02d0396e531c69e2c36e1851011b616aeb3a4
Darren Boyd <darren.boyd@gmail.com>
2023-02-03 13:50:17 -0800

    Move toward postcss without sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('@csstools/postcss-sass'),
+    require('postcss-sassy-mixins'),
+    require('postcss-nested'),
+    require('postcss-custom-properties')({
+      preserve: false
+    }),
+    require('postcss-hexrgba'),
+    require('postcss-preset-env'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
