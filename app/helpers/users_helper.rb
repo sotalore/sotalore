ecc04c7c31bbf06db97a07683461c52f9d33400d
Darren Boyd <darren.boyd@gmail.com>
2023-02-06 09:45:09 -0800

    Move recipes/show to tailwind

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,7 +7,12 @@ module UsersHelper
   ROLE_COLORS = {
     'root' => 'bg-purple-500',
     'editor' => 'bg-orange-500'
-  }
+  }.freeze
+
+  ROLE_NAMES = {
+    'root' => 'developer',
+    'editor' => 'editor',
+  }.freeze
 
   def user_flair_tag(user)
     if user.has_role?('root')
@@ -18,7 +23,7 @@ module UsersHelper
 
     if role
       color_class = ROLE_COLORS[role] || 'bg-grey-500'
-      content_tag(:span, role, class: "#{STANDARD_CLASSES} #{color_class}")
+      content_tag(:span, ROLE_NAMES[role], class: "#{STANDARD_CLASSES} #{color_class}")
     end
   end
 
