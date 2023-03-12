c6a39dfea93ca7fb907537b7b62f30a76dce0def
Darren Boyd <darren.boyd@gmail.com>
2023-03-12 09:40:29 -0700

    Farming calendar considers location factor

--- spec/requests/farming_calendars_spec.rb
+++ spec/requests/farming_calendars_spec.rb
@@ -3,8 +3,18 @@ require 'rails_helper'
 RSpec.describe "FarmingCalendars", type: :request do
   describe "GET /index" do
     it 'works' do
-      get farming_calendar_path(start: Time.zone.now, seedTime: 8, locationFactor: 1.0, name: 'Farm Watering')
+      start_time = Time.zone.now
+      get farming_calendar_path(start: start_time, seedTime: 8, locationFactor: 10, name: 'Farm Watering')
       expect(response).to have_http_status(200)
+
+      body = response.body
+      body.split
+      starts = body.split.select { |l| l.starts_with?('DTSTART') }
+      expect(starts.size).to eq(4)
+      expect(starts[0]).to eq("DTSTART:#{start_time.strftime('%Y%m%dT%H%M%S')}")
+      expect(starts[1]).to eq("DTSTART:#{(start_time + 80.hours).strftime('%Y%m%dT%H%M%S')}")
+      expect(starts[2]).to eq("DTSTART:#{(start_time + 160.hours).strftime('%Y%m%dT%H%M%S')}")
+      expect(starts[3]).to eq("DTSTART:#{(start_time + 240.hours).strftime('%Y%m%dT%H%M%S')}")
     end
   end
 end
