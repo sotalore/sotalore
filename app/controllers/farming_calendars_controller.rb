fbaf736aec47ed62f5be212f9bcbfecb1682f758
Darren Boyd <darren.boyd@gmail.com>
2023-03-12 09:09:18 -0700

    Adjust farming calendar for better UX

--- app/controllers/farming_calendars_controller.rb
+++ app/controllers/farming_calendars_controller.rb
@@ -6,38 +6,30 @@ class FarmingCalendarsController < ApplicationController
     base_time = Integer(params[:seedTime])
     location_factor = Float(params[:locationFactor])
     name = params[:name]
-    name = name.gsub(/[^0-9A-Za-z-_ ]/, '')
-    name = 'Farm Watering' if name.blank?
+    name = name.gsub(/[^0-9A-Za-z\-_ ]/, '')
+    name = 'SOTA Farming' if name.blank?
 
     cal = Icalendar::Calendar.new
-    cal.event do |e|
-      e.dtstart     = Icalendar::Values::DateTime.new(start_time)
-      e.dtend       = Icalendar::Values::DateTime.new(start_time + (base_time).hours)
-      e.summary     = "#{name} Phase 1"
-      e.ip_class    = "PRIVATE"
-    end
-    cal.event do |e|
-      e.dtstart     = Icalendar::Values::DateTime.new(start_time + (base_time).hours)
-      e.dtend       = Icalendar::Values::DateTime.new(start_time + (base_time * 2).hours)
-      e.summary     = "#{name} Phase 2"
-      e.ip_class    = "PRIVATE"
-      e.alarm do |a|
-        a.summary = "#{name} Phase 2"
-        a.trigger = "-PT1M" # 1 minute before
-      end
-    end
-    cal.event do |e|
-      e.dtstart     = Icalendar::Values::DateTime.new(start_time + (base_time * 2).hours)
-      e.dtend       = Icalendar::Values::DateTime.new(start_time + (base_time * 3).hours)
-      e.summary     = "#{name} Phase 3"
-      e.ip_class    = "PRIVATE"
-      e.alarm do |a|
-        a.summary = "#{name} Phase 3"
-        a.trigger = "-PT1M" # 1 minute before
-      end
-    end
-
+    add_event(cal, "#{name} Planting / Phase 1", start_time)
+    add_event(cal, "#{name} Watering / Phase 2", start_time + (base_time).hours, with_alarm: true)
+    add_event(cal, "#{name} Watering / Phase 3", start_time + (base_time * 2).hours, with_alarm: true)
+    add_event(cal, "#{name} Harvest", start_time + (base_time * 3).hours, with_alarm: true)
     cal.publish
     send_data cal.to_ical, type: 'text/calendar', disposition: 'attachment', filename: "#{name}.ics"
+  end
+
+  def add_event(calendar, name, start_time, with_alarm: false)
+    calendar.event do |e|
+      e.dtstart     = Icalendar::Values::DateTime.new(start_time)
+      e.dtend       = Icalendar::Values::DateTime.new(start_time + 30.minutes)
+      e.summary     = name
+      e.ip_class    = "PRIVATE"
+      if with_alarm
+        e.alarm do |a|
+          a.summary = name
+          a.trigger = "-PT1M" # 1 minute before
+        end
+      end
+    end
   end
 end
