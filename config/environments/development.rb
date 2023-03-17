8d6731987010dd986f8d8dfae9136aab3a6ad031
Darren Boyd <darren.boyd@gmail.com>
2023-03-17 15:04:37 -0700

    Move development storage back to local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
-  config.active_storage.service = :cloudflare_r2
+  config.active_storage.service = :local
 
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
