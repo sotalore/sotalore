b2d604dd458194f1e4c4cfd0047084988d2a957b
Darren Boyd <darren.boyd@gmail.com>
2023-03-23 11:31:27 -0700

    Better specs for UserPolicy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,11 +1,25 @@
 class UserPolicy < ApplicationPolicy
 
+  class Scope < Scope
+    def resolve
+      if user.has_role?('root')
+        scope.all
+      else
+        scope.where(id: user.id)
+      end
+    end
+  end
+
   def show?
     @user == @record or super
   end
 
   def update?
     @user == @record or super
+  end
+
+  def destroy?
+    false
   end
 
   protected
