b6485b43b2a7ffaee9ed677cbb847cd0539a5a3c
Darren Boyd <darren.boyd@gmail.com>
2023-03-30 15:27:38 -0700

    Move user/user-recipes to request specs

--- spec/requests/user/user_recipes_spec.rb
+++ spec/requests/user/user_recipes_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe User::UserRecipesController, type: :controller do
-
+RSpec.describe "User::UserRecipes", type: :request do
   let(:user) { create :user }
   before { sign_in user }
 
-  let(:recipe) { create :recipe }
+  let!(:recipe) { create :recipe }
 
   describe 'POST create' do
     it 'adds a UserRecipe' do
-      expect { post :create, xhr: true, params: { recipe_id: recipe.id } }
+      expect { post user_user_recipes_path, params: { recipe_id: recipe.id } }
         .to change { UserRecipe.count }.by(1)
     end
 
     it 'just returns when nothing is found' do
-      post :create, xhr: true, params: { recipe_id: 0 }
+      post user_user_recipes_path, params: { recipe_id: 0 }
       expect(response).to have_http_status(:not_acceptable)
     end
   end
@@ -23,14 +22,14 @@ RSpec.describe User::UserRecipesController, type: :controller do
     let!(:existing) { user.user_recipes.create(recipe: recipe) }
     describe 'POST create' do
       it 'simply succeeds' do
-        post :create, xhr: true, params: { recipe_id: recipe.id }
+        post user_user_recipes_path, params: { recipe_id: recipe.id }
         expect(response).to have_http_status(:ok)
       end
     end
 
     describe 'DELETE destroy' do
       it 'removes the UserRecipe' do
-        delete :destroy, xhr: true, params: { id: existing, recipe_id: recipe.id }
+        delete user_user_recipe_path(existing), params: { recipe_id: recipe.id }
         expect(response).to have_http_status(:ok)
       end
     end
