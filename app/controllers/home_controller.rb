6a85148d596687122f3dd9c9b6e6f9cca0063c12
Darren Boyd <darren.boyd@gmail.com>
2023-03-31 14:20:31 -0700

    Tweaks around layout and search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_after_action :verify_authorized
 
   def show
-    @top_post = TopPost.find_by(key: 'what-to-do-next')
+    @top_post = TopPost.find_or_create_by(key: 'what-to-do-next')
   end
 
   def roadmap
