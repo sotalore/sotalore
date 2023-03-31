17b2d750d5b25298bbb69301c07bf806573afa47
Darren Boyd <darren.boyd@gmail.com>
2023-03-31 08:28:12 -0700

    Fill out items request specs for regular users

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
-  let(:current_user) { create :user, :root }
-  before { sign_in current_user }
 
-  it_behaves_like "a standard root-only editable resource" do
-    let(:model) { Item }
-    let(:fixture) { :item }
-    let(:invalid_attributes) { { name: '' } }
+  context 'Given current_user is root' do
+    let(:current_user) { create :user, :root }
+    before { sign_in current_user }
+
+    it_behaves_like "an editable resource" do
+      let(:model) { Item }
+      let(:fixture) { :item }
+      let(:invalid_attributes) { { name: '' } }
+    end
+  end
+
+  context 'Given current_user is a regular user' do
+    let(:current_user) { create :user }
+    before { sign_in current_user }
+
+    it_behaves_like "a non-editable resource" do
+      let(:model) { Item }
+      let(:fixture) { :item }
+    end
   end
 
   describe 'GET by_use' do
