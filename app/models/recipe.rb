2db89b3ba446056e806082fdc5ea32d8d96746a2
Darren Boyd <darren.boyd@gmail.com>
2023-03-31 11:30:16 -0700

    Better Recipe.random

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -24,11 +24,8 @@ class Recipe < ApplicationRecord
   scope :by_name, -> { order(Arel.sql('lower(name)')) }
 
   def self.random(count=1)
-    max_id = maximum(:id)
-    return [] if max_id.nil?
-
-    max_id -= count
-    where('id > ?', rand(0..max_id)).take(count)
+    ids = Recipe.all.pluck(:id).sample(count)
+    where(id: ids)
   end
 
   before_validation :set_recipe_key
