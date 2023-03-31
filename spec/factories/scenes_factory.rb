e0a5ddd9279f7040ec233b4f1bd54340c37a1782
Darren Boyd <darren.boyd@gmail.com>
2023-03-31 08:27:18 -0700

    Fill out scenes spec for all users

--- spec/factories/scenes_factory.rb
+++ spec/factories/scenes_factory.rb
@@ -3,6 +3,18 @@ FactoryBot.define do
     sequence(:name) { |i| "Scene #{i}" }
     level_id { 3 }
     scene_type_id { 1 }
-    region_id {  1 }
+    region_id { 1 }
+    parent_id do
+      if name == 'Overworld'
+        nil
+      else
+        (Scene.find_by(name: 'Overworld') || create(:scene, :overworld)).id
+      end
+    end
+
+    trait :overworld do
+      name { "Overworld" }
+      parent_id { nil }
+    end
   end
 end
--- spec/requests/scenes_spec.rb
+++ spec/requests/scenes_spec.rb
@@ -1,37 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe "Scenes", type: :request do
-  let(:current_user) { create :user, :root }
-  before { sign_in current_user }
 
-  describe "DELETE /scenes/:id" do
-    let!(:scene) { create :scene }
+  let!(:overworld) { create :scene, :overworld }
 
-    it "won't delete a scene" do
-      # we don't support this ... yet
-      delete scene_path(scene)
-      expect(response).to redirect_to(scene_path(scene))
-      expect(scene.reload).to be_present
-    end
+  context 'Given current_user is root' do
+    let(:current_user) { create :user, :root }
+    before { sign_in current_user }
 
-    context 'Given an attachment_id' do
-      let(:image) { file_fixture('hawk-on-agave.jpg') }
-      before do
-        scene.images.attach(io: File.open(image), filename: 'hawk-on-agave.jpg', content_type: 'image/jpeg')
-      end
+    describe "DELETE /scenes/:id" do
+      let!(:scene) { create :scene, parent: overworld }
 
-      it 'deletes the attachment' do
-        delete scene_path(scene, attachment_id: scene.images.first.id)
+      it "won't delete a scene" do
+        # we don't support this ... yet
+        delete scene_path(scene)
         expect(response).to redirect_to(scene_path(scene))
-        expect(scene.reload.images).to be_empty
+        expect(scene.reload).to be_present
       end
+
+      context 'Given an attachment_id' do
+        let(:image) { file_fixture('hawk-on-agave.jpg') }
+        before do
+          scene.images.attach(io: File.open(image), filename: 'hawk-on-agave.jpg', content_type: 'image/jpeg')
+        end
+
+        it 'deletes the attachment' do
+          delete scene_path(scene, attachment_id: scene.images.first.id)
+          expect(response).to redirect_to(scene_path(scene))
+          expect(scene.reload.images).to be_empty
+        end
+      end
+    end
+
+    it_behaves_like "an editable resource", except: [:destroy] do
+      let(:model) { Scene }
+      let(:fixture) { :scene }
+      let(:invalid_attributes) { { name: '' } }
     end
   end
 
-  it_behaves_like "a standard root-only editable resource", except: [:destroy] do
-    let(:model) { Scene }
-    let(:fixture) { :scene }
-    let(:invalid_attributes) { { name: '' } }
+  context 'Given current_user is a regular user' do
+    it_behaves_like "a non-editable resource" do
+      let(:model) { Scene }
+      let(:fixture) { :scene }
+      let(:invalid_attributes) { { name: '' } }
+    end
   end
-
 end
