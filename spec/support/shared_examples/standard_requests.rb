5efc258e7bcecce2c1fc6b17e463ce1a4cf3983b
Darren Boyd <darren.boyd@gmail.com>
2023-03-31 08:25:02 -0700

    More better standard request shared specs

--- spec/support/shared_examples/standard_requests.rb
+++ spec/support/shared_examples/standard_requests.rb
@@ -1,11 +1,12 @@
-RSpec.shared_examples "a standard root-only editable resource" do |except: []|
+RSpec.shared_examples "an editable resource" do |except: []|
 
   let(:valid_params) { { model.model_name.param_key => attributes_for(fixture) } }
   let(:invalid_params) { { model.model_name.param_key => invalid_attributes } }
 
   unless except.include? :index
+    before { create_list fixture, 3 }
     describe 'GET index' do
-      it 'works' do
+      it 'renders the index' do
         get url_for(model)
         expect(response).to have_http_status(:ok)
       end
@@ -14,7 +15,7 @@ RSpec.shared_examples "a standard root-only editable resource" do |except: []|
 
   unless except.include? :new
     describe 'GET new' do
-      it 'works' do
+      it 'renders new' do
         get new_polymorphic_path(model)
         expect(response).to have_http_status(:ok)
       end
@@ -39,7 +40,7 @@ RSpec.shared_examples "a standard root-only editable resource" do |except: []|
 
   unless except.include? :show
     describe 'GET show' do
-      it 'works' do
+      it 'shows the resource' do
         get polymorphic_path(create fixture)
         expect(response).to have_http_status(:ok)
       end
@@ -48,7 +49,7 @@ RSpec.shared_examples "a standard root-only editable resource" do |except: []|
 
   unless except.include? :edit
     describe 'GET edit' do
-      it 'works' do
+      it 'edits the resource' do
         get edit_polymorphic_path(create fixture)
         expect(response).to have_http_status(:ok)
       end
@@ -73,10 +74,86 @@ RSpec.shared_examples "a standard root-only editable resource" do |except: []|
   unless except.include? :destroy
     describe 'DELETE destroy' do
       subject { create fixture }
-      it "deletes the subject" do
+      it "deletes the resource" do
         delete polymorphic_path(subject)
         expect(response).to redirect_to url_for(model)
         expect { subject.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+end
+
+RSpec.shared_examples "a non-editable resource" do |except: []|
+
+  let(:valid_params) { { model.model_name.param_key => attributes_for(fixture) } }
+
+  unless except.include? :index
+    before { create fixture }
+    describe 'GET index' do
+      it 'renders the index' do
+        get url_for(model)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  unless except.include? :new
+    describe 'GET new' do
+      it 'forbids access to new' do
+        get new_polymorphic_path(model)
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  unless except.include? :create
+    describe 'POST create' do
+      it 'forbids access to create' do
+        expect {
+          post polymorphic_path(model), params: valid_params
+        }.to_not change { model.count }
+        expect(response).to redirect_to root_path
+      end
+
+    end
+  end
+
+  unless except.include? :show
+    describe 'GET show' do
+      it 'shows the resource' do
+        get polymorphic_path(create fixture)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  unless except.include? :edit
+    describe 'GET edit' do
+      it 'forbids access to edit' do
+        get edit_polymorphic_path(create fixture)
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  unless except.include? :update
+    describe 'POST update' do
+      subject { create fixture }
+      it 'forbids access to update' do
+        patch polymorphic_path(subject), params: valid_params
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  unless except.include? :destroy
+    describe 'DELETE destroy' do
+      subject { create fixture }
+      it "forbids access to destroy" do
+        delete polymorphic_path(subject)
+        expect(response).to redirect_to root_path
+        expect { subject.reload }.to_not raise_error
       end
     end
   end
