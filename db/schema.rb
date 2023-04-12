436761f40c68e25445afcb94e8b374f0447669f0
Darren Boyd <darren.boyd@gmail.com>
2023-04-12 12:46:53 -0700

    Cleanup errant table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -53,18 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_191959) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
   create_table "avatars", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name", null: false
