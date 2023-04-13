3b6f12c02da9e1377899f8760c0fdf2427247779
Darren Boyd <darren.boyd@gmail.com>
2023-04-13 09:04:45 -0700

    More tweaks to astronomy to correct constellations

--- app/models/constellations.rb
+++ app/models/constellations.rb
@@ -2,22 +2,22 @@
 
 class Constellations
 
-  ORBITAL_PERIOD = 6 * Clock::NB_MONTH
+  ORBITAL_PERIOD = Clock::NB_YEAR
 
   # assumes January Chalice is north
   ALL = {
-    chalice: { offset: 330,   virtue: 'Honor',        city: 'Kiln' },
-    tear:    { offset: 300, virtue: 'Sacrifice',    city: 'Northwood' },
-    scales:  { offset: 270, virtue: 'Justice',      city: 'Jaanaford' },
-    sword:   { offset: 240, virtue: 'Valor',        city: 'Point West' },
-    heart:   { offset: 210, virtue: 'Compassion',   city: 'Brookside' },
-    hand:    { offset: 180, virtue: 'Honesty',      city: 'Etceter' },
-    ethos:   { offset: 150, virtue: '',             city: '' },
-    bell:    { offset: 120, virtue: 'Courage',      city: 'Resolute' },
-    candle:  { offset: 90, virtue: 'Love',         city: 'Ardoris' },
-    book:    { offset: 60,  virtue: 'Truth',        city: 'Aerie' },
-    crook:   { offset: 30,  virtue: 'Humility',     city: 'Eastmarch' },
-    ankh:    { offset: 0,  virtue: 'Spirituality', city: 'Fortus End' },
+    chalice: { offset: 195, virtue: 'Honor',        city: 'Kiln' },
+    tear:    { offset: 165, virtue: 'Sacrifice',    city: 'Northwood' },
+    scales:  { offset: 135, virtue: 'Justice',      city: 'Jaanaford' },
+    sword:   { offset: 105, virtue: 'Valor',        city: 'Point West' },
+    heart:   { offset: 75, virtue: 'Compassion',   city: 'Brookside' },
+    hand:    { offset: 45, virtue: 'Honesty',      city: 'Etceter' },
+    ethos:   { offset: 15,  virtue: '',             city: '' },
+    bell:    { offset: 345,  virtue: 'Courage',      city: 'Resolute' },
+    candle:  { offset: 315,  virtue: 'Love',         city: 'Ardoris' },
+    book:    { offset: 285, virtue: 'Truth',        city: 'Aerie' },
+    crook:   { offset: 255, virtue: 'Humility',     city: 'Eastmarch' },
+    ankh:    { offset: 225, virtue: 'Spirituality', city: 'Fortus End' },
   }
 
 end
