530278fd2ce48ea8feb78e05c335493c8c1db31e
Darren Boyd <darren.boyd@gmail.com>
2023-09-01 12:02:51 -0700

    Tweak and cleanup around basic main content layout

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -6,13 +6,14 @@ module LayoutHelper
     sm: 'max-w-screen-sm',
     md: 'max-w-screen-md',
     lg: 'max-w-screen-lg',
-    xl: 'max-w-screen-xl'
+    xl: 'max-w-screen-xl',
+    full: 'max-w-full',
   }
 
   def layout_main_content(size: :md, centered: false)
     raise ArgumentError, "Invalid size: #{size}" unless SIZE_CLASSES.key?(size)
 
-    classes = [SIZE_CLASSES[size], "m-4"]
+    classes = [SIZE_CLASSES[size], "mx-2 my-4"]
     classes << 'mx-auto' if centered
 
     content_tag(:div, class: classes.join(" ")) do
