b58766c0e4ea893483bd925881f0960f5d5de9f6
Darren Boyd <darren.boyd@gmail.com>
2023-09-18 09:35:01 -0700

    Tweak skill ignore-toggle

--- app/helpers/skills_helper.rb
+++ app/helpers/skills_helper.rb
@@ -6,4 +6,19 @@ module SkillsHelper
 
     skills.select { |s| !avatar.ignoring_skill?(s) }
   end
+
+  def toggle_skill_button(avatar, skill)
+    return unless avatar
+
+    if @avatar.ignoring_skill?(skill)
+      icon = render_icon('eye_slash', size: :sm)
+      path = reveal_avatar_skill_path(@avatar, skill.id)
+    else
+      icon = render_icon('eye', size: :sm)
+      show_all_param = params.key?(:show_all) ? { show_all: true } : {}
+      path = ignore_avatar_skill_path(@avatar, skill.id, **show_all_param)
+    end
+
+    button_to(icon, path, class: 'text-slorange-500', method: :patch, tabindex: "-1", form: { class: 'inline'})
+  end
 end
