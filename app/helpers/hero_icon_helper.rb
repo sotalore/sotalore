769ed5a791bd9d010a8597980e913596f3bb469b
Darren Boyd <darren.boyd@gmail.com>
2024-03-15 16:47:06 -0700

    Move recipes.css over to tailwind

--- app/helpers/hero_icon_helper.rb
+++ app/helpers/hero_icon_helper.rb
@@ -40,7 +40,7 @@ module HeroIconHelper
     svg = ICONS[name]
     size = SIZES[size]
     color = COLORS[color]
-    content_tag(:span, svg, class: "inline-block #{color} #{size} #{css_class}")
+    content_tag(:span, svg, class: "inline-flex items-center #{color} #{size} #{css_class}")
   end
 
 # <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -360,7 +360,7 @@ module HeroIconHelper
     </svg>
   ICON
   add_icon :user_group, USER_GROUP
- 
+
   EYE = <<~ICON.html_safe
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline">
       <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
