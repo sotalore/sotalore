ec0461739571bc4a48c8bc218dcc840626782908
Darren Boyd <darren.boyd@gmail.com>
2024-11-05 16:35:02 -0800

    Remove postcss-hexrgba

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,6 @@ module.exports = {
     require('postcss-custom-properties')({
       preserve: false
     }),
-    require('postcss-hexrgba'),
     require('postcss-preset-env'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
