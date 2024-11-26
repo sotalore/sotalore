dc7674039eb785333f91f1a26df4ec5da2dd4d60
Darren Boyd <darren.boyd@gmail.com>
2024-11-26 09:20:44 -0800

    Enable lograge in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.lograge.enabled = true
 
   # Prevent health checks from clogging up the logs.
   config.silence_healthcheck_path = "/up"
