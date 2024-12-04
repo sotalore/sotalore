f430a0dbf522c7725c0c1d5fc2ffb45135aab8c3
Darren Boyd <darren.boyd@gmail.com>
2024-12-04 10:04:27 -0800

    Add and setup Logtail in production

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem "image_processing", ">= 1.2"
 gem "aws-sdk-s3", require: false
 
 gem 'lograge'
+gem 'logtail-rails'
 
 gem 'bootsnap'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  # config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.logger = Logtail::Logger.create_default_logger(ENV['LOGTAIL_SOURCE_TOKEN'])
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
