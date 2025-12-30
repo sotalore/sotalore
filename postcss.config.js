7f566ef6f39356f2b70e1ffbe8492d9559f5e37c
Darren Boyd <darren.boyd@gmail.com>
2025-12-30 11:21:17 -0800

    Cleanup old asset management configs

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-preset-env'),
-    require('tailwindcss/nesting'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
-}
